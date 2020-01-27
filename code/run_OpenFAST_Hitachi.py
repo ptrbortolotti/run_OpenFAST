5f8b168be905fbc4674baa6b93e0ad06d2d9ea0a
Hristo Ivanov <hivanov@el1.ib0.cm.hpc.nrel.gov>
2020-01-27 12:39:49 -0700

    backup run_Hitachi script

--- code/run_OpenFAST_Hitachi.py
+++ code/run_OpenFAST_Hitachi.py
@@ -1,23 +1,25 @@
 from wisdem.aeroelasticse.runFAST_pywrapper import runFAST_pywrapper, runFAST_pywrapper_batch
 from wisdem.aeroelasticse.CaseGen_IEC       import CaseGen_IEC
 
+import shutil
+
 if __name__=="__main__":
     
-    eagle = False
+    eagle = True
     
     iec = CaseGen_IEC()
     iec.Turbine_Class = 'I' # I, II, III, IV
     iec.Turbulence_Class = 'A'
     iec.D = 204.
-    iec.z_hub = 130.6
+    iec.z_hub = 131.5
     
     # adjust later
     # Turbine Data
     iec.init_cond = {} # can leave as {} if data not available
-    iec.init_cond[("ElastoDyn","RotSpeed")] = {'U':[3., 5., 7., 8.2, 25.]}
-    iec.init_cond[("ElastoDyn","RotSpeed")]['val'] = [2.92, 4.88, 6.81, 7.835, 7.88]
-    iec.init_cond[("ElastoDyn","BlPitch1")] = {'U':[3., 8.2,  9., 11.,  13.,  15.,  17., 19., 21., 23., 25]}
-    iec.init_cond[("ElastoDyn","BlPitch1")]['val'] = [0., 0., 2.93, 8.76, 12.01, 14.82, 17.37, 19.73, 21.96, 24.08, 26.10]
+    iec.init_cond[("ElastoDyn","RotSpeed")] = {'U':[4.000000000000000000e+00, 5.105263157894737169e+00, 6.210526315789474339e+00, 7.315789473684210620e+00, 8.421052631578948677e+00, 9.000000000000000000e+00, 2.500000000000000000e+01]}
+    iec.init_cond[("ElastoDyn","RotSpeed")]['val'] = [3.744822190397536943e+00, 4.779575690375803987e+00, 5.814329190354071919e+00, 6.849082690332338963e+00, 7.883836190310606007e+00, 8.425849928394459454e+00, 8.425849928394459454e+00]
+    iec.init_cond[("ElastoDyn","BlPitch1")] = {'U':[4.000000000000000000e+00, 1.066803743941258809e+01, 1.173684210526315752e+01, 1.284210526315789558e+01, 1.394736842105263186e+01, 1.505263157894736992e+01, 1.615789473684210620e+01, 1.726315789473684248e+01, 1.836842105263158231e+01, 1.947368421052631504e+01, 2.057894736842105488e+01, 2.168421052631579116e+01, 2.278947368421052744e+01, 2.389473684210526372e+01, 2.500000000000000000e+01]}
+    iec.init_cond[("ElastoDyn","BlPitch1")]['val'] = [0., 0., 5.074352971947472035e+00, 7.786747053509759375e+00, 9.942305156633004515e+00, 1.182455292927485146e+01, 1.353567522224319397e+01, 1.512176953395546164e+01, 1.661022980833649498e+01, 1.801929396075015077e+01, 1.936157283370897986e+01, 2.064702870035782922e+01, 2.188383170827142266e+01, 2.307840321137150852e+01, 2.423572382317490437e+01]
     iec.init_cond[("ElastoDyn","BlPitch2")] = iec.init_cond[("ElastoDyn","BlPitch1")]
     iec.init_cond[("ElastoDyn","BlPitch3")] = iec.init_cond[("ElastoDyn","BlPitch1")]
     
@@ -35,7 +37,7 @@ if __name__=="__main__":
     iec.transient_shear_orientation = 'both'  # 'v','h','both': vertical or horizontal shear for EWS
 
     # Naming, file management, etc
-    iec.wind_dir = 'outputs/wind'
+    iec.wind_dir = 'outputs/wind_Hitachi'
     iec.case_name_base = 'Hitachi'
     if eagle:
         iec.Turbsim_exe = '/projects/windse/importance_sampling/WT_Codes/Turbsim/TurbSim/bin/TurbSim_glin64'
@@ -46,7 +48,7 @@ if __name__=="__main__":
     
     iec.debug_level = 2
     iec.parallel_windfile_gen = True
-    iec.run_dir = 'outputs/OpenFAST'
+    iec.run_dir = 'outputs/200114_baseline_up'
 
     # Run case generator / wind file writing
     case_inputs = {}
@@ -56,13 +58,15 @@ if __name__=="__main__":
     # Run FAST cases
     fastBatch = runFAST_pywrapper_batch(FAST_ver='OpenFAST',dev_branch = True)
     if eagle:
-        fastBatch.FAST_exe = '/home/pbortolo/OpenFAST/build/glue-codes/openfast/openfast'   # Path to executable
+        fastBatch.FAST_exe = '/projects/windse/hitachi/openfast/build/glue-codes/openfast/openfast'   # Path to executable
         fastBatch.FAST_InputFile = 'Baseline_Hit10MW.fst'   # FAST input file (ext=.fst)
-        fastBatch.FAST_directory = '/home/pbortolo/wisdem_1_0_0/models_OpenFAST/BAR_00'   # Path to fst directory files
+        fastBatch.FAST_directory = '/projects/windse/hitachi/Hitachi_Design/OpenFAST/Baseline_Hit10MW_upwind'   # Path to fst directory files
     else:
         fastBatch.FAST_exe = '/mnt/c/GitHub/OpenFAST/build/glue-codes/openfast/openfast'   # Path to executable
         fastBatch.FAST_InputFile = 'Baseline_Hit10MW.fst'   # FAST input file (ext=.fst)
         fastBatch.FAST_directory = '/mnt/c/GitHub/Hitachi_Design/OpenFAST/Baseline_Hit10MW/'   # Path to fst directory files
+    shutil.copyfile(fastBatch.FAST_directory + '/DISCON.IN',iec.run_dir + '/DISCON.IN')
+    shutil.copyfile(fastBatch.FAST_directory + '/Cp_Ct_Cq_Hit10MW_v14.txt',iec.run_dir + '/Cp_Ct_Cq_Hit10MW_v14.txt')
     fastBatch.FAST_runDirectory = iec.run_dir
     fastBatch.case_list = case_list
     fastBatch.case_name_list = case_name_list
