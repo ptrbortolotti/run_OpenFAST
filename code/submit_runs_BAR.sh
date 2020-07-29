9404cf4225ee8213798aaf2e57e503f9618649ae
Pietro Bortolotti <pbortolo@el3.ib0.cm.hpc.nrel.gov>
2020-07-29 14:53:27 -0600

    run mpi sbatch

--- code/submit_runs_BAR.sh
+++ code/submit_runs_BAR.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #SBATCH --account=bar
-#SBATCH --time=0-04:00:00
-#SBATCH --job-name=bar00
-#SBATCH --nodes=1
-#SBATCH --ntasks=36
+#SBATCH --time=0-24:00:00
+#SBATCH --job-name=bar02
+#SBATCH --nodes=7
 #SBATCH --mail-user pbortolo@nrel.gov
 #SBATCH --mail-type BEGIN,END,FAIL
-#SBATCH --qos=high
+####SBATCH --partition=debug
+####SBATCH --qos=high
+######SBATCH --mem=1000GB      # RAM in MB
 #SBATCH --output=job_log.%j.out  # %j will be replaced with the job ID
 
 module purge
@@ -17,4 +18,4 @@ module load gcc/8.2.0
 source deactivate
 conda activate wisdem-env
 
-python run_OpenFAST_BAR00.py
+mpirun -np 250 python run_OpenFAST_BAR02.py
